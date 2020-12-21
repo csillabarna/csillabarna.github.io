4f811082f2a480ebea6f1169eeb50479a7d1381a
Csilla Barna <barnacsilla89@gmail.com>
2020-12-21 16:50:43 +0000

    nice menu items

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const menuBtn = document.querySelector('.menu-btn')
 const burger = document.querySelector('.menu-btn__burger')
 const nav = document.querySelector('.nav')
 const menuNav = document.querySelector('.menu-nav')
-
+const navItems = Array.from(document.querySelectorAll('.menu-nav__item'))
 
 let showMenu = false
 
@@ -11,13 +11,13 @@ function toggleMenu() {
     burger.classList.add('open')
     nav.classList.add('open')
     menuNav.classList.add('open')
-   
+    navItems.forEach(item =>item.classList.add('open'))
     showMenu = true
   } else {
     burger.classList.remove('open')
     nav.classList.remove('open')
     menuNav.classList.remove('open')
-    
+    navItems.forEach(item =>item.classList.remove('open'))
     showMenu = false
   }
 }
