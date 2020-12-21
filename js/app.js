ed86905b48bca6d80b342e9c3ad98b2a4a4dd446
Csilla Barna <barnacsilla89@gmail.com>
2020-12-21 15:59:49 +0000

    nav is in the burger

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn')
 const burger = document.querySelector('.menu-btn__burger')
-
+const nav = document.querySelector('.nav')
+const menuNav = document.querySelector('.menu-nav')
 
 
 let showMenu = false
@@ -8,10 +9,14 @@ let showMenu = false
 function toggleMenu() {
   if (!showMenu){
     burger.classList.add('open')
+    nav.classList.add('open')
+    menuNav.classList.add('open')
    
     showMenu = true
   } else {
     burger.classList.remove('open')
+    nav.classList.remove('open')
+    menuNav.classList.remove('open')
     
     showMenu = false
   }
